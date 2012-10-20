449fa32f80e4e1ef1c35a16b460fe920677a1aad
Nicolai Ommer <nicolai.ommer@gmail.com>
2012-10-20 21:43:09 +0200

    Added entity classes generated by JPA reverse engineering

---
+++ src/edu/dhbw/oodb/Customer.java
@@ -0,0 +1,108 @@
+package edu.dhbw.oodb;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * The persistent class for the CUSTOMER database table.
+ * 
+ */
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "C_CUSTKEY", unique = true, nullable = false, precision = 22)
+	private long cCustkey;
+
+	@Column(name = "C_ACCTBAL", nullable = false, precision = 15, scale = 2)
+	private BigDecimal cAcctbal;
+
+	@Column(name = "C_ADDRESS", nullable = false, length = 40)
+	private String cAddress;
+
+	@Column(name = "C_COMMENT", nullable = false, length = 117)
+	private String cComment;
+
+	@Column(name = "C_MKTSEGMENT", nullable = false, length = 10)
+	private String cMktsegment;
+
+	@Column(name = "C_NAME", nullable = false, length = 25)
+	private String cName;
+
+	@Column(name = "C_NATIONKEY", nullable = false, precision = 22)
+	private BigDecimal cNationkey;
+
+	@Column(name = "C_PHONE", nullable = false, length = 15)
+	private String cPhone;
+
+	public Customer() {
+	}
+
+	public long getCCustkey() {
+		return this.cCustkey;
+	}
+
+	public void setCCustkey(long cCustkey) {
+		this.cCustkey = cCustkey;
+	}
+
+	public BigDecimal getCAcctbal() {
+		return this.cAcctbal;
+	}
+
+	public void setCAcctbal(BigDecimal cAcctbal) {
+		this.cAcctbal = cAcctbal;
+	}
+
+	public String getCAddress() {
+		return this.cAddress;
+	}
+
+	public void setCAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+
+	public String getCComment() {
+		return this.cComment;
+	}
+
+	public void setCComment(String cComment) {
+		this.cComment = cComment;
+	}
+
+	public String getCMktsegment() {
+		return this.cMktsegment;
+	}
+
+	public void setCMktsegment(String cMktsegment) {
+		this.cMktsegment = cMktsegment;
+	}
+
+	public String getCName() {
+		return this.cName;
+	}
+
+	public void setCName(String cName) {
+		this.cName = cName;
+	}
+
+	public BigDecimal getCNationkey() {
+		return this.cNationkey;
+	}
+
+	public void setCNationkey(BigDecimal cNationkey) {
+		this.cNationkey = cNationkey;
+	}
+
+	public String getCPhone() {
+		return this.cPhone;
+	}
+
+	public void setCPhone(String cPhone) {
+		this.cPhone = cPhone;
+	}
+
+}
---
+++ src/edu/dhbw/oodb/Order.java
@@ -0,0 +1,121 @@
+package edu.dhbw.oodb;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * The persistent class for the ORDERS database table.
+ * 
+ */
+@Entity
+@Table(name = "ORDERS")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "O_ORDERKEY", unique = true, nullable = false, precision = 22)
+	private long oOrderkey;
+
+	@Column(name = "O_CLERK", nullable = false, length = 15)
+	private String oClerk;
+
+	@Column(name = "O_COMMENT", nullable = false, length = 79)
+	private String oComment;
+
+	@Column(name = "O_CUSTKEY", nullable = false, precision = 22)
+	private BigDecimal oCustkey;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "O_ORDERDATE", nullable = false)
+	private Date oOrderdate;
+
+	@Column(name = "O_ORDERPRIORITY", nullable = false, length = 15)
+	private String oOrderpriority;
+
+	@Column(name = "O_ORDERSTATUS", nullable = false, length = 1)
+	private String oOrderstatus;
+
+	@Column(name = "O_SHIPPRIORITY", nullable = false, precision = 22)
+	private BigDecimal oShippriority;
+
+	@Column(name = "O_TOTALPRICE", nullable = false, precision = 15, scale = 2)
+	private BigDecimal oTotalprice;
+
+	public Order() {
+	}
+
+	public long getOOrderkey() {
+		return this.oOrderkey;
+	}
+
+	public void setOOrderkey(long oOrderkey) {
+		this.oOrderkey = oOrderkey;
+	}
+
+	public String getOClerk() {
+		return this.oClerk;
+	}
+
+	public void setOClerk(String oClerk) {
+		this.oClerk = oClerk;
+	}
+
+	public String getOComment() {
+		return this.oComment;
+	}
+
+	public void setOComment(String oComment) {
+		this.oComment = oComment;
+	}
+
+	public BigDecimal getOCustkey() {
+		return this.oCustkey;
+	}
+
+	public void setOCustkey(BigDecimal oCustkey) {
+		this.oCustkey = oCustkey;
+	}
+
+	public Date getOOrderdate() {
+		return this.oOrderdate;
+	}
+
+	public void setOOrderdate(Date oOrderdate) {
+		this.oOrderdate = oOrderdate;
+	}
+
+	public String getOOrderpriority() {
+		return this.oOrderpriority;
+	}
+
+	public void setOOrderpriority(String oOrderpriority) {
+		this.oOrderpriority = oOrderpriority;
+	}
+
+	public String getOOrderstatus() {
+		return this.oOrderstatus;
+	}
+
+	public void setOOrderstatus(String oOrderstatus) {
+		this.oOrderstatus = oOrderstatus;
+	}
+
+	public BigDecimal getOShippriority() {
+		return this.oShippriority;
+	}
+
+	public void setOShippriority(BigDecimal oShippriority) {
+		this.oShippriority = oShippriority;
+	}
+
+	public BigDecimal getOTotalprice() {
+		return this.oTotalprice;
+	}
+
+	public void setOTotalprice(BigDecimal oTotalprice) {
+		this.oTotalprice = oTotalprice;
+	}
+
+}
