7333c1fcd9d5fd77d814ead6c989e4bacbf6d002
Felix Pistorius <mail@felix-pistorius.de>
2012-10-21 21:09:33 +0200

    JDBC DAO

---
+++ src/JDBC/JdbcDao.java
@@ -0,0 +1,18 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcDao {
+	
+	private static final String url="jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String user="uni";
+	private static final String passwd="uni";
+
+	public Connection getConnection() throws SQLException {
+		Connection connection = DriverManager.getConnection(url, user, passwd);
+		return connection;
+	}
+
+}
