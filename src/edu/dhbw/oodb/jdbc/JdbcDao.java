da1e550c0fe1f858d60d31d46c9215b6090a9bc9
Felix Pistorius <mail@felix-pistorius.de>
2012-10-21 21:12:44 +0200

    jdbc dao

--- src/edu/dhbw/oodb/jdbc/JdbcDao.java
+++ src/edu/dhbw/oodb/jdbc/JdbcDao.java
@@ -1,11 +1,11 @@
-package JDBC;
+package edu.dhbw.oodb.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcDao {
-	
+
 	private static final String url="jdbc:oracle:thin:@localhost:1521:XE";
 	private static final String user="uni";
 	private static final String passwd="uni";
@@ -14,5 +14,5 @@ public class JdbcDao {
 		Connection connection = DriverManager.getConnection(url, user, passwd);
 		return connection;
 	}
-
+	
 }
