5133d5f2afe10996facd8864d4931465e792d869
Daniel Andres <andreslopez.daniel@gmail.com>
2012-10-29 14:48:00 +0100

    benchmarkfiles changed

--- test/src/edu/dhbw/oodb/PerfidixContainer.java
+++ test/src/edu/dhbw/oodb/PerfidixContainer.java
@@ -9,41 +9,36 @@ import org.perfidix.ouput.CSVOutput;
 import org.perfidix.ouput.TabularSummaryOutput;
 import org.perfidix.result.BenchmarkResult;
 
+public class PerfidixContainer {
+	final static String[] classNames = {
+			"edu.dhbw.oodb.dao.test.AbstractDaoTests",
+			"edu.dhbw.oodb.dao.test.CustomerDaoTest",
+			"edu.dhbw.oodb.dao.test.OrderDaoTest" };
 
-public class PerfidixContainer
-{
-	final static String[]	classNames	= { "edu.dhbw.sos.gui.plan.data.TimeBlocksTest" };
-	
-	
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
-		
+	public static void main(String[] args) {
+
 		BenchmarkResult result;
-		try
-		{
+		try {
 			result = Perfidix.runBenchs(classNames);
-			File folder = new File(System.getProperty("user.home") + "/workspace/OODB_FetchPerf/benchmark/");
+			File folder = new File(System.getProperty("user.home")
+					+ "/workspace/OODB_FetchPerf/benchmark/");
 			PrintStream sum = new PrintStream(System.getProperty("user.home")
 					+ "/workspace/OODB_FetchPerf/benchmark/summary");
 			new TabularSummaryOutput().visitBenchmark(result);
 			new TabularSummaryOutput(sum).visitBenchmark(result);
 			new CSVOutput(folder).visitBenchmark(result);
-		} catch (ClassNotFoundException err)
-		{
+		} catch (ClassNotFoundException err) {
 			err.printStackTrace();
-		} catch (InstantiationException err)
-		{
+		} catch (InstantiationException err) {
 			err.printStackTrace();
-		} catch (IllegalAccessException err)
-		{
+		} catch (IllegalAccessException err) {
 			err.printStackTrace();
-		} catch (FileNotFoundException err)
-		{
+		} catch (FileNotFoundException err) {
 			err.printStackTrace();
 		}
 	}
-	
+
 }
